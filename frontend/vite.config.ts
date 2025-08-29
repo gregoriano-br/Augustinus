0cf41b84259a78abe0bfc37738f2a21cc75869db
Giovanni Del Chiaro <GigioDC@proton.me>
2025-08-29 20:38:50 -0300

    frontend bug

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,22 @@
 import { defineConfig } from 'vite';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
   optimizeDeps: {
     include: ['separador-silabas'],
   },
-  build: {
-    assetsInclude: ['**/*.js'],
-  },
-  publicDir: '../',
+  plugins: [
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'exsurge.min.js',
+          dest: ''
+        },
+        {
+          src: 'exsurge.min.js.map',
+          dest: ''
+        }
+      ]
+    })
+  ]
 });
