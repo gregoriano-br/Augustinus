5cacbedb97849ea2f5a4f973283407ec741eb8bd
SonyNeto <sonyneto@gmail.com>
2025-10-05 02:41:42 -0300

    Add new psalm models and enhance applyModel function for improved note positioning

--- packages/core/src/psalms.ts
+++ packages/core/src/psalms.ts
@@ -21,7 +21,32 @@ function replaceFromEnd(input: string, find: string, replaceWith: string, limit?
     return result;
 }
 
-function applyModel(lyrics: string, gabcModel: string): string {
+// Lógica de posicionamento das notas nas sílabas de um salmo
+function psalmLogic(input: string[], notes: string[]) { //Função que aplica a lógica no array de sílabas com as tônicas marcadas com #
+    const i = input.length;
+    const tonicNote = notes.filter(note => note.includes("r1")).reverse().map(note => note.replace("r1", "")); // Procura pela nota da tônica melódica
+    const replaceAt = (index: number, value: string) => {input[index] = input[index].replace("@", value)}; // Função menor, parecida com a replaceFromEnd, mas para array
+    const isTonic = (index: number): boolean => input[index]?.includes("#") ?? false; // Função que será usada mais tarde
+    const tonicIndex = i - input.findLastIndex(syllable => syllable.includes("#")); // Procura pelo índice da primeira sílaba tônica de trás pra frente
+    switch (tonicIndex) {
+        case 1:
+            replaceAt(i - 1, tonicNote[0].replace(")", ""));
+            input[i - 1] += notes[notes.length - 1].replace("(", "");
+            break;
+        case 2:
+            replaceAt(i - 2, tonicNote[0]);
+            replaceAt(i - 1, notes[notes.length - 1]);
+            break;
+
+        case 3:
+            replaceAt(i - 3, tonicNote[1]? tonicNote[1] : tonicNote[0]);
+            replaceAt(i - 2, notes[notes.length - 2]);
+            replaceAt(i - 1, notes[notes.length - 1]);
+            break;
+    }  
+};
+
+function applyModel(lyrics: string, gabcModel: string, psalm: boolean): string {
     const taggedParts: string[] = [];
     const placeholder = "||TAGGED_PART||";
     
@@ -64,32 +89,52 @@ function applyModel(lyrics: string, gabcModel: string): string {
     let pause: string = notes.pop() || "";
     gabcOutput += wordsWithNotePlaceholders.join(" ");
     let gabcOutputArray: string[] = gabcOutput.split(/(?<=@)/);
-    // Lógica de posicionamento das notas nas sílabas nova
-    const psalmLogic = (input: string[], notes: string[]) => { //Função que aplica a lógica no array de sílabas com as tônicas marcadas com #
-        const i = input.length;
-        const tonicNote = notes.filter(note => note.includes("'")).reverse(); // Procura pela nota da tônica melódica
-        const replaceAt = (index: number, value: string) => {input[index] = input[index].replace("@", value)}; // Função menor, parecida com a replaceFromEnd, mas para array
-        const isTonic = (index: number): boolean => input[index]?.includes("#") ?? false; // Função que será usada mais tarde
-        const tonicIndex = i - input.findLastIndex(syllable => syllable.includes("#")); // Procura pelo índice da primeira sílaba tônica de trás pra frente
-        switch (tonicIndex) {
-            case 1:
-                replaceAt(i - 1, tonicNote[0].replace(")", ""));
-                input[i - 1] += notes[notes.length - 1].replace("(", "");
-                break;
-            case 2:
-                replaceAt(i - 2, tonicNote[0]);
-                replaceAt(i - 1, notes[notes.length - 1]);
-                break;
 
-            case 3:
-                replaceAt(i - 3, tonicNote[1]? tonicNote[1] : tonicNote[0]);
-                replaceAt(i - 2, notes[notes.length - 2]);
-                replaceAt(i - 1, notes[notes.length - 1]);
-                break;
-        }  
-    };
+    if (isDynamic && lastWordForTonic) {
+        if (psalm) {
+            // Separa as notas do sufixo em dois grupos de acentos
+            let firstAccentIndex = notes.findIndex(note => note.includes("r1"));
+            let secondAccentIndex = notes.findIndex((note, i) => i > firstAccentIndex && note.includes("r1"));
+            let preNotesIndex = (firstAccentIndex - 1) >= 0 ? (firstAccentIndex - 1) : false;
+            // Se só tiver um acento, mantém um único grupo
 
-    if (/*isDynamic &&*/ lastWordForTonic) {
+            let firstAccentNotes = secondAccentIndex === -1 ? notes.slice(firstAccentIndex) : notes.slice(firstAccentIndex, secondAccentIndex);
+            let secondAccentNotes = secondAccentIndex === -1 ? [] : notes.slice(secondAccentIndex);
+            let preNotes = preNotesIndex !== false ? notes.slice(0, preNotesIndex + 1) : false;
+
+            // Se tiver dois grupos, o secondAccentNotes é aplicado primeiro, depois cortado do array, o firstAccentNotes é aplicado ao que sobrou e os dois arrays são concatenados
+            if (secondAccentIndex !== -1) {
+                psalmLogic(gabcOutputArray, secondAccentNotes);
+                const firstAccentGabcIndex = gabcOutputArray.findIndex(syllable => syllable.includes("("));
+
+                const firstAccentGabc = gabcOutputArray.slice(firstAccentGabcIndex);
+                const secondAccentGabc = gabcOutputArray.slice(0, firstAccentGabcIndex);
+
+                psalmLogic(secondAccentGabc, firstAccentNotes);
+
+                gabcOutputArray = secondAccentGabc.concat(firstAccentGabc);
+            }
+            // Se tiver só um grupo, aplica o firstAccentNotes normalmente
+            else {
+                psalmLogic(gabcOutputArray, firstAccentNotes);
+            }
+            // Se tiver um grupo de notas prévias, aplica às sílabas restantes
+            if (preNotes){
+                for (let i = preNotes.length - 1, j = gabcOutputArray.length - 1; i >= 0 && j >= 0; j--) {
+                    if (!gabcOutputArray[j].includes("(")) {
+                        gabcOutputArray[j] = gabcOutputArray[j].replace("@", preNotes[i]);
+                        i--;
+                    }
+                }
+            }
+        // Limpa os caracteres de marcação
+        gabcOutputArray = gabcOutputArray.map(syllable => syllable.replace(/#|(?<=\()'/g, ""));
+
+        // Junta o array com o GABC numa string e adiciona a pausa no final
+        gabcOutputArray.push(pause);
+        gabcOutput = gabcOutputArray.join("");
+        }
+        
         const tonicNumber: number = tonic(lastWordForTonic.split("@"));
         let offset: number = 0;
         for (let i = 0; i < notes.length; i++) {
@@ -101,50 +146,9 @@ function applyModel(lyrics: string, gabcModel: string): string {
         for (let i = 0; i < notes.length; i++) {
             notes[i] = notes[i]?.replace("r1", "").replace("r", "") || "";
         }
-        // Separa as notas do sufixo em dois grupos de acentos
-        let firstAccentIndex = notes.findIndex(note => note.includes("'"));
-        let secondAccentIndex = notes.findIndex((note, i) => i > firstAccentIndex && note.includes("'"));
-        let preNotesIndex = (firstAccentIndex - 1) >= 0 ? (firstAccentIndex - 1) : false;
-        // Se só tiver um acento, mantém um único grupo
 
-        let firstAccentNotes = secondAccentIndex === -1 ? notes.slice(firstAccentIndex) : notes.slice(firstAccentIndex, secondAccentIndex);
-        let secondAccentNotes = secondAccentIndex === -1 ? [] : notes.slice(secondAccentIndex);
-        let preNotes = preNotesIndex !== false ? notes.slice(0, preNotesIndex + 1) : false;
-
-        // Se tiver dois grupos, o secondAccentNotes é aplicado primeiro, depois cortado do array, o firstAccentNotes é aplicado ao que sobrou e os dois arrays são concatenados
-        if (secondAccentIndex !== -1) {
-            psalmLogic(gabcOutputArray, secondAccentNotes);
-            const firstAccentGabcIndex = gabcOutputArray.findIndex(syllable => syllable.includes("("));
-
-            const firstAccentGabc = gabcOutputArray.slice(firstAccentGabcIndex);
-            const secondAccentGabc = gabcOutputArray.slice(0, firstAccentGabcIndex);
-
-            psalmLogic(secondAccentGabc, firstAccentNotes);
-
-            gabcOutputArray = secondAccentGabc.concat(firstAccentGabc);
-        }
-        // Se tiver só um grupo, aplica o firstAccentNotes normalmente
-        else {
-            psalmLogic(gabcOutputArray, firstAccentNotes);
-        }
-        // Se tiver um grupo de notas prévias, aplica às sílabas restantes
-        if (preNotes){
-            for (let i = preNotes.length - 1, j = gabcOutputArray.length - 1; i >= 0 && j >= 0; j--) {
-                if (!gabcOutputArray[j].includes("(")) {
-                    gabcOutputArray[j] = gabcOutputArray[j].replace("@", preNotes[i]);
-                    i--;
-                }
-            }
-        }
-        // Limpa os caracteres de marcação
-        gabcOutputArray = gabcOutputArray.map(syllable => syllable.replace(/#|(?<=\()'/g, ""));
-
-        // Junta o array com o GABC numa string e adiciona a pausa no final
-        gabcOutputArray.push(pause);
-        gabcOutput = gabcOutputArray.join("");
-
-        // Lógica de posicionamento das notas nas sílabas anterior
-        /* switch (tonicNumber) {
+        if (!psalm){
+            switch (tonicNumber) {
             case 1:
                 const joined_note: string = ((notes[0 + offset] || "").slice(0, -1) + (notes[2 + offset] || "").substring(1)).replaceAll(/([a-m])\1/g, "$1");
                 gabcOutput = replaceFromEnd(gabcOutput, "@", joined_note, 1);
@@ -158,13 +162,16 @@ function applyModel(lyrics: string, gabcModel: string): string {
                 gabcOutput = replaceFromEnd(gabcOutput, "@", notes[1 + offset] || "", 1);
                 gabcOutput = replaceFromEnd(gabcOutput, "@", notes[0 + offset] || "", 1);
                 break;
+            }
+
+            for (let i = offset - 1; i >= 0; i--) {
+                gabcOutput = replaceFromEnd(gabcOutput, "@", notes[i] || "", 1);
+            }
+            for (let i = offset + 3; i < notes.length; i++) {
+                gabcOutput += " " + notes[i];
+            }
         }
-        for (let i = offset - 1; i >= 0; i--) {
-            gabcOutput = replaceFromEnd(gabcOutput, "@", notes[i] || "", 1);
-        }
-        for (let i = offset + 3; i < notes.length; i++) {
-            gabcOutput += " " + notes[i];
-        } */
+        
     }
     else{
         for (let i = notes.length - 2; i >= 0; i--) {
@@ -216,6 +223,8 @@ export interface Parameters {
 export default function generateGabc(input: string, modelObject: Model, parametersObject: Parameters): string {
     let model = { ...modelObject };
 
+    let psalm = model.type === "salmo"? true : false;
+
     if (model.type === 'custom') {
         if (model.tom === 'simples') {
             const note = parametersObject.customNote || 'h';
@@ -260,7 +269,7 @@ export default function generateGabc(input: string, modelObject: Model, paramete
             if (replacement !== undefined) {
                 gabcLines.push(replacement);
             } else {
-                gabcLines.push(applyModel(chunk, model.default));
+                gabcLines.push(applyModel(chunk, model.default, psalm));
             }
             continue;
         }
@@ -269,9 +278,9 @@ export default function generateGabc(input: string, modelObject: Model, paramete
         const pattern = model.patterns.find(p => p.symbol === lastChar);
         if (pattern) {
             const text = model.type === 'evangelho' ? chunk.trim() : chunk.slice(0, -1).trim();
-            gabcLines.push(applyModel(text, pattern.gabc));
+            gabcLines.push(applyModel(text, pattern.gabc, psalm));
         } else {
-            gabcLines.push(applyModel(chunk.trim() + (parametersObject.removeSeparator === false ? parametersObject.separator : ''), model.default));
+            gabcLines.push(applyModel(chunk.trim() + (parametersObject.removeSeparator === false ? parametersObject.separator : ''), model.default, psalm));
         }
     }
 
@@ -295,12 +304,12 @@ export { defaultModels };
 
 //let lyrics = "Não aceita o conselho dos ímpios"
 //let lyrics = "O Senhor é fiel para sempre,"
-//let lyrics = "Ó Sião, o teu Deus reinará";
+let lyrics = "Ó Sião, o teu Deus reinará";
 //let gabcModel = "(j) (jr jr jr) ('k) (jr) (j.)"
 //let gabcModel = "(h) (hr hr hr) ('i) (gr) (g) ('h) (fr) (f.)"
 //let gabcModel = "(h) (hr hr hr) (g) (f) ('gh) (gr) (gvFED.)"
 //let gabcModel = "(h) (hr hr hr) (g) ('e) (fr) (f.)"
-//let gabcModel = "(j) (jr jr jr) ('k) (jr) (j) ('jr) ('ih) (j.)"
+//let gabcModel = "(j) (jr jr jr) (kr1) (jr) (j) (jr1) (ihr1) (j.) (::)"
 //console.log(applyModel(lyrics, gabcModel))
 
 console.log(generateGabc("Não aceita + o conselho dos ímpios * Bendito seja o nome do Senhor \n O Senhor é fiel para sempre, Ó Sião, * o teu Deus reinará", defaultModels[0], {separator: "|", removeSeparator: true, addOptionalStart: false, addOptionalEnd: false, removeNumbers: false, removeParenthesis: false}));
